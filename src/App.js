85dbd3c2c55252e22878d14945d44203b5b6b098
Adam Jones <domdomegg+git@gmail.com>
2021-09-30 11:29:37 +0200

    Create example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { Dialog } from '@headlessui/react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isPageLong, setIsPageLong] = useState(true);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: '2rem', padding: '1rem', outline: '0.5rem solid black', height: isPageLong ? '110vh' : '20vh' }}>
+      <p>isPageLong: {String(isPageLong)}</p>
+      <button onClick={() => setIsPageLong(!isPageLong)}>Toggle page length</button>
+      <button onClick={() => setIsDialogOpen(true)}>Open dialog/modal</button>
+      <Dialog
+        as="div"
+        style={{ position: 'fixed', top: '0', bottom: '0', left: '0', right: '0' }}
+        open={isDialogOpen}
+        onClose={() => setIsDialogOpen(false)}
+      >
+        <Dialog.Overlay
+          style={{ position: 'fixed', top: '0', bottom: '0', left: '0', right: '0', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+          />
+
+        <div style={{ margin: '2rem', padding: '1rem', backgroundColor: 'white', position: 'relative', zIndex: 10 }} className="p-8 my-16 bg-white rounded shadow relative z-10 overflow-auto">
+          <Dialog.Title>I am the modal content</Dialog.Title>
+          <Dialog.Description>I want to match up with the outline behind (which I am {isPageLong && 'not'} doing now)</Dialog.Description>
+        </div>
+      </Dialog>
     </div>
   );
 }
